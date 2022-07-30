db2ffa1901f42353bbeaf8fef214b1d97b0664ca
wojciechkubiak <wgkubiak@gmail.com>
2022-07-30 20:10:44 +0200

    Exercise basic timer

---
+++ models/Timer.ts
@@ -0,0 +1,48 @@
+export enum TimerState {
+  PAUSED,
+  STARTED,
+  STOPPED,
+}
+
+export class Timer {
+  timerState: TimerState;
+  millisecondsLeft: number;
+  timeout: NodeJS.Timer | undefined;
+  isCompleted: boolean;
+
+  constructor(
+    timerState: TimerState,
+    millisecondsLeft: number = 36000,
+    isCompleted: boolean = false
+  ) {
+    this.timerState = timerState;
+    this.millisecondsLeft = millisecondsLeft;
+    this.isCompleted = isCompleted;
+  }
+
+  pause() {
+    this.timerState = TimerState.PAUSED;
+    clearInterval(this.timeout);
+  }
+
+  start() {
+    this.timerState = TimerState.STARTED;
+
+    this.timeout = setTimeout(() => {
+      if (this.millisecondsLeft - 1000 <= 0) {
+        this.timerState = TimerState.STOPPED;
+        this.millisecondsLeft = 0;
+        this.isCompleted = true;
+        clearInterval(this.timeout);
+      } else {
+        this.millisecondsLeft -= 1000;
+      }
+    }, 1000);
+  }
+
+  stop(timerDuration: number = 36000) {
+    this.timerState = TimerState.STOPPED;
+    this.millisecondsLeft = timerDuration;
+    clearInterval(this.timeout);
+  }
+}
--- pages/Exercise.tsx
+++ pages/Exercise.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
-import { ScreenNames } from "../models/Page";
+import { useState, useEffect } from "react";
+import { Button, Text, View } from "react-native";
+import BackgroundTimer from "react-native-background-timer";
 
 const Exercise = () => {
+  const [timer, setTimer] = useState<number>(36000);
+  const tm = BackgroundTimer;
+
+  console.log(tm);
+  useEffect(() => {
+    const loadInitTimer = () => setTimer(16000);
+
+    loadInitTimer();
+  }, []);
+
+  const handleStart = () => {
+    console.log(tm);
+    tm.runBackgroundTimer(() => {
+      setTimer(timer - 1000);
+    }, 1000);
+  };
+
   return (
     <View>
-      <Text>{ScreenNames.Exercise}</Text>
+      <Text>{timer}</Text>
+      <Button title="Start" onPress={handleStart} />
     </View>
   );
 };
